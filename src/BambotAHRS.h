53d3f7d58ecfbe4da4244b8f44a3e6de96d241f1
Greg Armstrong <garmstrong456@gmail.com>
2018-06-26 22:22:29 -0400

    initial commit

---
+++ src/BambotAHRS.h
@@ -0,0 +1,69 @@
+//=============================================================================================
+// BambotAHRS.h
+//
+// Adapted from Paul Stoffregen's MahonyAHRS Library by Greg Armstrong, July 2018
+// See https://github.com/PaulStoffregen/MahonyAHRS for the original
+//=============================================================================================
+//
+// Madgwick's implementation of Mayhony's AHRS algorithm.
+// See: http://www.x-io.co.uk/open-source-imu-and-ahrs-algorithms/
+//
+// Date			Author			Notes
+// 29/09/2011	SOH Madgwick    Initial release
+// 02/10/2011	SOH Madgwick	Optimised for reduced CPU load
+//
+//=============================================================================================
+#ifndef BambotAHRS_h
+#define BambotAHRS_h
+#include <math.h>
+
+//--------------------------------------------------------------------------------------------
+// Variable declaration
+
+class Mahony {
+private:
+	float twoKp;		// 2 * proportional gain (Kp)
+	float twoKi;		// 2 * integral gain (Ki)
+	float q0, q1, q2, q3;	// quaternion of sensor frame relative to auxiliary frame
+	float integralFBx, integralFBy, integralFBz;  // integral error terms scaled by Ki
+	float invSampleFreq;
+	float roll, pitch, yaw;
+	char anglesComputed;
+	static float invSqrt(float x);
+	void computeAngles();
+
+//-------------------------------------------------------------------------------------------
+// Function declarations
+
+public:
+	Mahony();
+	void begin(float sampleFrequency) { invSampleFreq = 1.0f / sampleFrequency; }
+	void update(float gx, float gy, float gz, float ax, float ay, float az, float mx, float my, float mz, float deltaT);
+	void updateIMU(float gx, float gy, float gz, float ax, float ay, float az, float deltaT);
+	float getRoll() {
+		if (!anglesComputed) computeAngles();
+		return roll * 57.29578f;
+	}
+	float getPitch() {
+		if (!anglesComputed) computeAngles();
+		return pitch * 57.29578f;
+	}
+	float getYaw() {
+		if (!anglesComputed) computeAngles();
+		return yaw * 57.29578f + 180.0f;
+	}
+	float getRollRadians() {
+		if (!anglesComputed) computeAngles();
+		return roll;
+	}
+	float getPitchRadians() {
+		if (!anglesComputed) computeAngles();
+		return pitch;
+	}
+	float getYawRadians() {
+		if (!anglesComputed) computeAngles();
+		return yaw;
+	}
+};
+
+#endif
